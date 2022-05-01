f2f817ce495b45efb7450d46332e14100327c88f
Bugo <bugo@dragomano.ru>
2022-05-01 12:37:49 +0500

    Add spanish_es

---
+++ Themes/default/languages/SimTopics/.spanish_es.php
@@ -0,0 +1,45 @@
+<?php
+
+$txt['similar_topics']           = 'Temas Similares';
+$txt['simtopics_desc']           = 'Desde aqui puede configurar las distintas opciones del mod: Similar Topics. You can also buy <a class="bbc_link" href="https://www.buymeacoffee.com/bugo">a cup of coffee</a> for the developer.';
+$txt['simtopics_settings']       = 'Configuracion Similar Topics';
+$txt['simtopics_num_topics']     = 'Maximo de temas a mostrar';
+$txt['simtopics_nt_desc']        = '0 para desactivar el mod.';
+$txt['simtopics_only_cur_board'] = 'Buscar temas similares solo del mismo foro';
+$txt['simtopics_ocb_desc']       = 'Deseleccionar si desea buscar temas similares en todos los foros.';
+$txt['simtopics_ignored_boards'] = 'Foros ignorados';
+$txt['simtopics_when_new_topic'] = 'Mostrar temas similares cuando se esta creando un nuevo tema';
+$txt['simtopics_on_display']     = 'Display list of similar topics inside these topics';
+
+$txt['simtopics_displayed_columns'] = 'Displayed columns';
+
+$txt['simtopics_position']          = 'Where to display the block of similar topics';
+$txt['simtopics_position_variants'] = array('Above topic', 'Below topic');
+
+$txt['simtopics_sorting']          = 'Sorting type';
+$txt['simtopics_sorting_variants'] = array(
+    'By relevance',
+    'By relevance (descending)',
+    'By title',
+    'By title (descending)',
+    'By creation time',
+    'By creation time (descending)',
+    'By last post time',
+    'By last post time (descending)'
+);
+
+$txt['simtopics_cache_int']   = 'Intervalo de actualizacion de cache';
+$txt['simtopics_ci_post']     = 'segundos.';
+$txt['groups_simtopics_view'] = $txt['group_perms_name_simtopics_view'] = 'Permisos para ver temas similares';
+$txt['groups_simtopics_post'] = $txt['group_perms_name_simtopics_post'] = 'Permisos para ver temas similares en nuevo tema';
+
+$txt['simtopics_ignored_boards'] = 'Ignored boards';
+
+$txt['permissiongroup_simtopics']     = $txt['permissiongroup_simple_simtopics'] = $txt['similar_topics'];
+$txt['permissionname_simtopics_view'] = 'Visualizacion de la lista de temas similares';
+$txt['permissionhelp_simtopics_view'] = 'Con este permiso los usuarios pueden ver la lista de temas similares.';
+$txt['permissionname_simtopics_post'] = 'Visualizacion de la lista de temas similares al crear nuevo tema';
+$txt['permissionhelp_simtopics_post'] = 'Cuando los usuarios estan creando un nuevo tema podran ver una lista de temas similares.';
+
+$txt['simtopics_no_result']  = 'No existen temas similares.';
+$txt['simtopics_no_subject'] = 'Escriba un titulo por favor.';
